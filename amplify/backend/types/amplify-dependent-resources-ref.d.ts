5c1fd266753a33e77856ea22badccce1af12be38
ayebravo <ayelenbravo@gmail.com>
2022-04-07 19:09:20 -0500

    Ran amplify push
    Pushes the auth service created locally to the cloud

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "basicauthentication09921745": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
